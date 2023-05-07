a9ae970c142987aedebd5e90dfc23a8e133252fa
Jo Chong <jo3cyc@gmail.com>
2023-05-07 19:55:59 +0800

    param working

--- app/index.js
+++ app/index.js
@@ -1,18 +1,48 @@
-import { Link, Stack } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { useEffect } from "react";
+import {
+  Link,
+  Stack,
+  useNavigation,
+  useRouter,
+  useSearchParams,
+} from "expo-router";
+import {
+  Image,
+  Text,
+  View,
+  FlatList,
+  Pressable,
+  StyleSheet,
+} from "react-native";
 
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
-    />
-  );
-}
+const items = [
+  { id: "1", title: "Item 1" },
+  { id: "2", title: "Item 2" },
+  { id: "3", title: "Item 3" },
+];
 
 export default function Home() {
+  const { post } = useSearchParams();
+  const router = useRouter();
+
+  const renderItem = ({ item }) => (
+    <Pressable
+      style={styles.itemContainer}
+      onPress={() => router.push("details", { title: item.title })}
+    >
+      <Text style={styles.itemText}>{item.title}</Text>
+    </Pressable>
+  );
+
+  useEffect(() => {
+    if (post) {
+      // Post updated, do something with `post`
+      // For example, send the post to the server
+    }
+  }, [post]);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <Stack.Screen
         options={{
           // https://reactnavigation.org/docs/headers#setting-the-header-title
@@ -23,8 +53,6 @@ export default function Home() {
           headerTitleStyle: {
             fontWeight: "bold",
           },
-          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
-          headerTitle: (props) => <LogoTitle {...props} />,
         }}
       />
 
@@ -33,6 +61,34 @@ export default function Home() {
       <Link href={{ pathname: "details", params: { name: "Bacon" } }}>
         Go to Details
       </Link>
+      <Text styles={styles.header}>List of artwork</Text>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  itemContainer: {
+    backgroundColor: "blue",
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  itemText: {
+    color: "white",
+  },
+});
